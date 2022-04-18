87800b38778aaac003bfadd093057555af910529
Bolohonov <bolohonov@list.ru>
2022-04-18 22:39:32 +0400

    feat:Add sprint_8 homework

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,21 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+    private int id;
+
+    @NotBlank(message = "name may not be null")
+    private String name;
+
+    private String description;
+    private LocalDate releaseDate;
+    private Duration duration;
+}
