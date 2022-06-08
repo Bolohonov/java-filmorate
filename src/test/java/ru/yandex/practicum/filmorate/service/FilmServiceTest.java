b1c1bdc88dafea15f2d7a6545f8b689d906ff3aa
EgorZhuganov <egor-zhuganov@yandex.ru>
2022-06-09 02:18:27 +1000

    style: отступы

--- src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/FilmServiceTest.java
@@ -33,14 +33,14 @@ class FilmServiceTest {
     private final FilmService filmService;
     private final UserService userService;
     private final DirectorService directorService;
-  
-  @Test
+
+    @Test
     void getFilmsByDirectorSortedByLike() {
         Director director = directorService.createDirector(Director.builder().name("Dir1").build());
         List<Film> filmList = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             filmList.add(filmService.addFilm(Film.builder()
-                                             .name("Film" + i)
+                    .name("Film" + i)
                     .description("Descr" + i)
                     .releaseDate(of(2021, 12, 2))
                     .duration(ofSeconds(1500))
@@ -52,12 +52,12 @@ class FilmServiceTest {
         User user1 = User.builder().birthday(of(2000, 1, 1)).name("user1").login("login1").email("email1@ya.ru").build();
         User user2 = User.builder().birthday(of(2000, 1, 1)).name("user2").login("login2").email("email2@ya.ru").build();
         User user3 = User.builder().birthday(of(2000, 1, 1)).name("user3").login("login3").email("email3@ya.ru").build();
-      
+
         user1 = userService.addUser(user1);
         user2 = userService.addUser(user2);
         user3 = userService.addUser(user3);
-      
-      filmService.addLike(filmList.get(2).getId(), 1);
+
+        filmService.addLike(filmList.get(2).getId(), 1);
         filmService.addLike(filmList.get(1).getId(), 2);
         filmService.addLike(filmList.get(2).getId(), 3);
         filmService.addLike(filmList.get(2).getId(), 2);
@@ -92,31 +92,35 @@ class FilmServiceTest {
         assertThat(sortedList.get(1).getId()).isEqualTo(filmList.get(filmList.size() - 2).getId());
         assertThat(sortedList.get(2).getId()).isEqualTo(filmList.get(filmList.size() - 3).getId());
     }
-      
+
     @Test
     void test0getCommonFilmsIf2CommonFilmsExistingShouldReturn2FilmsFromList() {
         List<Film> filmList = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             filmList.add(filmService.addFilm(builder()
-                    .id(i)
+                    .name("Film" + i)
+                    .description("Descr" + i)
+                    .releaseDate(of(1950 + i, 12, 2))
+                    .duration(ofSeconds(1500))
+                    .mpa(new Mpa(1, "G"))
                     .build()));
         }
-        User user1 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user1").login("login1").email("email1@ya.ru").build();
-        User user2 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user2").login("login2").email("email2@ya.ru").build();
-        User user3 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user3").login("login3").email("email3@ya.ru").build();
+        User user1 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user1").login("login1").email("email1@ya.ru").build();
+        User user2 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user2").login("login2").email("email2@ya.ru").build();
+        User user3 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user3").login("login3").email("email3@ya.ru").build();
 
 
         user1 = userService.addUser(user1);
         user2 = userService.addUser(user2);
         user3 = userService.addUser(user3);
-       
-        filmService.addLike(filmList.get(0).getId(),1);
-        filmService.addLike(filmList.get(0).getId(),2);
-        filmService.addLike(filmList.get(0).getId(),3);
-        filmService.addLike(filmList.get(1).getId(),1);
-        filmService.addLike(filmList.get(1).getId(),2);
-        filmService.addLike(filmList.get(2).getId(),1);
-        filmService.addLike(filmList.get(3).getId(),2);
+
+        filmService.addLike(filmList.get(0).getId(), user1.getId());
+        filmService.addLike(filmList.get(0).getId(), user2.getId());
+        filmService.addLike(filmList.get(0).getId(), user3.getId());
+        filmService.addLike(filmList.get(1).getId(), user1.getId());
+        filmService.addLike(filmList.get(1).getId(), user2.getId());
+        filmService.addLike(filmList.get(2).getId(), user1.getId());
+        filmService.addLike(filmList.get(3).getId(), user2.getId());
 
         assertThat(filmService.getCommonFilms(user1.getId(), user2.getId())).hasSize(2);
     }
@@ -134,18 +138,18 @@ class FilmServiceTest {
                     .mpa(new Mpa(1, "G"))
                     .build()));
         }
-        User user1 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user1").login("login1").email("email1@ya.ru").build();
-        User user2 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user2").login("login2").email("email2@ya.ru").build();
-        User user3 = User.builder().birthday(LocalDate.of(2000,1,1)).name("user3").login("login3").email("email3@ya.ru").build();
+        User user1 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user1").login("login1").email("email1@ya.ru").build();
+        User user2 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user2").login("login2").email("email2@ya.ru").build();
+        User user3 = User.builder().birthday(LocalDate.of(2000, 1, 1)).name("user3").login("login3").email("email3@ya.ru").build();
 
         user1 = userService.addUser(user1);
         user2 = userService.addUser(user2);
         user3 = userService.addUser(user3);
 
-        filmService.addLike(filmList.get(0).getId(),1);
-        filmService.addLike(filmList.get(0).getId(),2);
-        filmService.addLike(filmList.get(1).getId(),1);
-        filmService.addLike(filmList.get(1).getId(),2);
+        filmService.addLike(filmList.get(0).getId(), user1.getId());
+        filmService.addLike(filmList.get(0).getId(), user2.getId());
+        filmService.addLike(filmList.get(1).getId(), user1.getId());
+        filmService.addLike(filmList.get(1).getId(), user2.getId());
 
         assertThat(filmService.getCommonFilms(user2.getId(), user3.getId())).hasSize(0);
     }
